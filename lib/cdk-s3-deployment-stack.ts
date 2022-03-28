9124123ce17e732c12748a3394ec29eaa6abf128
EC2 Default User <ec2-user@ip-172-31-20-19.us-east-2.compute.internal>
2022-03-28 19:21:51 +0000

    first commit

--- lib/cdk-s3-deployment-stack.ts
+++ lib/cdk-s3-deployment-stack.ts
@@ -2,15 +2,25 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment'
+import * as s3 from 'aws-cdk-lib/aws-s3'
+
 export class CdkS3DeploymentStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkS3DeploymentQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+        websiteIndexDocument: 'index.html',
+        //publicReadAccess: true
+    });
+
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+        sources: [s3deploy.Source.asset('./website-dist')],
+        destinationBucket: websiteBucket,
+        destinationKeyPrefix: 'web/static' // optional prefix in destination bucket
+    })
+    
   }
 }
